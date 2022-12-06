a25bdd2d97b2781ae6ed8621dc8d99d924211fc0
vivekkumar854301 <vivekkumar2020@vitbhopal.ac.in>
2022-12-06 15:50:51 +0530

    post

---
+++ How_many_unattempted_problems.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x;
+	cin>>y;
+	cout<<x-y<<endl;
+	return 0;
+}
---
+++ Reach the Target.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    cin>>y;
+	    cout<<x-y<<endl;
+	}
+	return 0;
+}
